26d4c6cd419da2b414cb7472e1506c6454e8823c
Aurélien Chevalier <funigtor@funigtor.fr>
2018-12-04 21:42:03 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const pug = require('pug')
+const redis = require('redis')
+const fs = require('fs')
+const app = express()
+const rank = redis.createClient()
+const bodyParser = require('body-parser')
+
+const maxExo = fs.readdirSync('exercices/').length
+const homepage = fs.readFileSync('teamname.html', "UTF-8")
+const send_nudes = pug.compileFile("exercice.pug")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', function (req, res) {
+  res.send(homepage)
+})
+
+app.post('/exercice/', function(req,res){
+    let teamname = req.body.teamname
+    //if ()
+    rank.get(teamname,function(err,reply){
+        let teamrank = Number(reply)
+        if (teamrank > maxExo){
+            res.send("Épreuve terminée")
+        } else if (teamrank <= 0){
+            res.send("Exercice inconnu")
+        } else
+        res.send(send_nudes({n:reply, exercice: exercice[teamrank].question, teamname: teamname}))
+    })
+})
+
+app.post('/validation', function(req,res){
+    let teamname = req.body.teamname
+    rank.get(teamname,function(err,reply){
+        if (req.body.password == exercice[reply].answer){
+            console.log("Equipe "+teamname+" termine l'exercice " + reply + " à " + Date())
+            rank.incr(teamname)
+        }
+    res.redirect(307,"/exercice")
+    })
+})
+
+app.post('/start', function(req,res){
+    let teamname = req.body.teamname
+    if (teamname == undefined)
+        res.redirect('/')
+    let teamrank = 1
+    rank.get(teamname,function(err,reply){
+        if (reply == null)
+            rank.set(teamname,"1")
+        else
+            teamrank = reply
+    })
+    res.redirect(307,'/exercice/')
+})
+
+var exercice = new Array()
+// Read JSON
+for (let i = 1; i <= maxExo; i++){
+    fs.readFile('exercices/'+i+'.json',function(err,data){
+        exercice[i] = JSON.parse(data)
+    })
+}
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
